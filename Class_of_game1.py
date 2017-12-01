f46d2b477575f73b3b2047a466ec4cd228499c9b
Dima Toropov <toropov0014@mail.ru>
2017-12-01 08:41:48 +0300

    rabota 01/12/17

--- Class_of_game1.py
+++ Class_of_game1.py
@@ -9,6 +9,7 @@ class Interv(object):
   self.koiff_stavki = 1
   self.dict_of_interv={}
   self.interval =-1
+  self.cicle =0
   #self.strategia = strat
  def bank(self, bank,genDict):
      if genDict['activ_game'][0] == 0: #проверка на начало игры
@@ -19,16 +20,32 @@ class Interv(object):
      if var_of_risk > var:
         print(' превышен лимит риска игры')
         return 1
- def set_inerv_of_obj(self,inter):
-	 self.interval= inter
- def namber_of_stavka(self, ):
-	 pass
- 
+ def set_interv_of_obj(self,inter):
+     self.interval= inter
+ def namber_of_stavka(self,steps_of_sesii,strateg):
+    if self.cicle == 0:
+      if (steps_of_sesii < strateg):
+         self.koiff_stavki=1
+      else:
+          self.cicle == 1
+    
+    if self.cicle == 1:
+        if (steps_of_sesii < strateg/2):
+            self.koiff_stavki = 2
+        else:
+            self.cicle == 2
+    if self.cicle == 3:
+        if (steps_of_sesii < strateg/2):
+            self.koiff_stavki = 4
+        else:
+            self.cicle == 4 # банк перестает терять
+    return self.koiff_stavki
 
 new_interv = Interv(25)
 new_interv2 = Interv(25)
+Interv.strategia=18
 print(new_interv.strategia)
 print(new_interv2.strategia)
-new_interv2.strategia=24
+Interv.strategia=18
 print(new_interv.strategia)
 print(new_interv2.strategia)
