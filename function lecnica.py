964b41bc47540b2038f4d3520a31d1159440c57b
Dima Toropov <toropov0014@mail.ru>
2018-02-15 08:30:17 +0300

    rabota 15/2/18

--- function lecnica.py
+++ function lecnica.py
@@ -1,21 +1,24 @@
 def lecnica():
-    dobaka =0
+    dobavka =0
     stavka =1
     postavleno = 0
     pribyl =0
     i=0
-    for i in range(108):
+    for i in range(1000):
         # postavleno = postavleno+stavka
-        if i< 37:
-           dobaka = dobaka+0.01
+        if i> -1 and i< 37 :
+           dobavka = dobavka+0.01
+     
            postavleno = postavleno + stavka + 0.01
-        if i> 36 and i<73:
-           dobaka = dobaka+0.02
+        if i> 36 and i<74:
+           dobavka = dobavka+0.02
            postavleno = postavleno + stavka + 0.02
-        if i> 72 and i<108:
-           dobaka = dobaka+0.03
+        if i> 75 and i<1000:
+           dobavka = dobavka+0.03
            postavleno = postavleno + stavka + 0.03
-
-        pribyl = (stavka+dobaka)*36
+        stavka2 =stavka+dobavka
+        print('stavka',stavka+dobavka)
+        pribyl = (stavka+dobavka)*36
+        
         print('shag:',i,' kolichestvo postavlennogo:',postavleno, '  kompensacija: ',pribyl, 'raznica: ', pribyl - postavleno)
 lecnica()
---
+++ function lecnica2.py
@@ -0,0 +1,37 @@
+def lecnica():
+    dobavka = 0
+    stavka = 1
+    postavleno = 0
+    pribyl = 0
+    i = 0
+    for i in range(81):
+        # postavleno = postavleno+stavka
+        if i > -1 and i < 36:
+            #dobavka = dobavka + 0.01
+            print('dobavka: ',dobavka )
+            postavleno = postavleno + stavka
+            pribyl = (stavka ) * 36
+        if i > 35 and i < 57:
+            dobavka = dobavka + 0.04
+            postavleno = postavleno + stavka + dobavka
+            pribyl = (stavka + dobavka) * 36
+        if i > 56 and i < 66:
+            dobavka = dobavka + 0.06
+            postavleno = postavleno + stavka + dobavka
+            pribyl = (stavka + dobavka) * 36
+        
+        if i > 65 and i < 80:
+            dobavka = dobavka + 0.06
+            postavleno = postavleno + stavka + dobavka
+            pribyl = (stavka + dobavka) * 36
+        
+        
+        stavka2 = stavka + dobavka
+        print('stavka', stavka + dobavka)
+        
+        
+        print('shag:', i, ' kolichestvo postavlennogo:', postavleno, '  kompensacija: ', pribyl, 'raznica: ',
+              round(pribyl - postavleno,2))
+
+
+lecnica()
